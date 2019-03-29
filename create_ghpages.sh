d91ed6cfa8648123baf7466f261b0015dad36f05
Joel Kim <kim.dohhyoung@gmail.com>
2019-03-29 14:20:13 +0900

    automatic commit

---
+++ create_ghpages.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+git add . -A
+git commit -m "automatic commit"
+git push origin korean
+
+git checkout -B gh-pages
+git rebase korean
+
+touch .nojekyll
+
+echo '!build/' >> .gitignore
+
+cd sphinx
+sphinx-build -D language='ko'source build/html
+cd ..
+
+git add . -A
+git commit -m "build"
+git push -f origin gh-pages
+git reset --hard HEAD
+git clean -f
+git checkout korean
+
+source deactivate
